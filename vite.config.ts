b3648819694ccd2e580babff07220fc05f72e9db
Artem Frolov <artfrol.af@gmail.com>
2025-04-09 03:03:03 +0300

    youtube

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/veyalab-website/",
+  base: "/veyalab-website",
   
   server: {
     host: "::",
